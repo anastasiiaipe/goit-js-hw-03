11d469ddcab92627fd0d285d8da5f9085560fa81
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2023-12-06 20:15:37 +0100

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,13 @@
 "use strict";
+function slugify(title) {
+  const titleLow = title.toLowerCase();
+  const message = titleLow.split(" ");
+  const slug = message.join("-");
+
+  return slug;
+}
+
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
